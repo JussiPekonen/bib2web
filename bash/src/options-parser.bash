3eb657142b678e06aae462725ff0ef8ac8d1b829
Jussi Pekonen <jussi@pekonen.cc>
2020-08-03 15:46:56 +0300

    bash: Add help option and print out the usage of the script

--- bash/src/options-parser.bash
+++ bash/src/options-parser.bash
@@ -7,6 +7,19 @@ source "${BIB2WEB_BASE_DIR}/logger.bash"
 # shellcheck source=./error-codes.bash
 source "${BIB2WEB_BASE_DIR}/error-codes.bash"
 
+# Function to print out the help of the script
+printHelp() {
+	local scriptName=$(basename $0)
+	printf "${scriptName}: Tool for converting a BibTeX file to a format that can be released online\n\n"
+	printf "Usage: ${scriptName} [-h|--help] [-f|--format format] file\n\n"
+	printf "Options:\n"
+	printf "  -h\t\t--help\t\tPrints this help text and exits.\n"
+	printf "  -f format\t--format format\tSets the output format. Supported formats: ${BIB2WEB_SUPPORTED_OUTPUT_FORMATS}.\n"
+	printf "\t\t\t\tDefault: ${BIB2WEB_DEFAULT_OUTPUT_FORMAT}. Unsupported formats are reverted to the default.\n"
+	printf "  file\t\t\t\tThe BibTeX file to be used as the input.\n"
+	exit 0
+}
+
 # Function to parse the user's options
 parseOptions() {
 	while [ "$#" -gt 0 ]; do
@@ -15,6 +28,9 @@ parseOptions() {
 					shift
 					BIB2WEB_OUTPUT_FORMAT=$(echo "$1" | "${BIB2WEB_AWK}" -f "${BIB2WEB_BASE_DIR}/awk/tolower.awk")
 					shift
+					;;
+				-h|--help)
+					printHelp
 					;;
 				*)
 					if [ "${BIB2WEB_BIBTEX_FILE}" == "" ]; then
@@ -42,8 +58,8 @@ optionsSanityCheck() {
 		"${BIB2WEB_OUTPUT_FORMAT_HTML}"|"${BIB2WEB_OUTPUT_FORMAT_JSON}")
 			;;
 		*)
-			warning "Unsupported output format. Reverting to html."
-			BIB2WEB_OUTPUT_FORMAT="${BIB2WEB_OUTPUT_FORMAT_HTML}"
+			warning "Unsupported output format. Reverting to ${BIB2WEB_DEFAULT_OUTPUT_FORMAT}."
+			BIB2WEB_OUTPUT_FORMAT="${BIB2WEB_DEFAULT_OUTPUT_FORMAT}"
 			;;
 	esac
 	return 0
