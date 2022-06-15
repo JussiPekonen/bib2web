e13c548d8ad970e1fddf8a912aecd5d7f1cf8db0
Jussi Pekonen <jussi@pekonen.cc>
2022-06-15 23:10:13 +0300

    cmake + bash: Refactor the shell tooling to generic macros and functions

--- cmake/shell-tools.cmake
+++ cmake/shell-tools.cmake
@@ -1,23 +1,49 @@
-# Pre-check that shunit2 is available
-execute_process(COMMAND which ARGS shunit2
-	OUTPUT_VARIABLE BIB2WEB_SHUNIT2
-	ERROR_QUIET)
-string(REGEX REPLACE "\n" "" BIB2WEB_SHUNIT2 "${BIB2WEB_SHUNIT2}")
-if(NOT EXISTS "${BIB2WEB_SHUNIT2}")
-	message("shunit2 does not exist. No Bash tests will be added to the build.")
-	set(BIB2WEB_SHUNIT2_EXISTS FALSE)
-else()
-	set(BIB2WEB_SHUNIT2_EXISTS TRUE)
-endif()
+include("${PROJECT_SOURCE_DIR}/cmake/check-tools.cmake")
 
-# Pre-check that shellchecks is available
-execute_process(COMMAND which ARGS shellcheck
-	OUTPUT_VARIABLE BIB2WEB_SHELLCHECK
-	ERROR_QUIET)
-string(REGEX REPLACE "\n" "" BIB2WEB_SHELLCHECK "${BIB2WEB_SHELLCHECK}")
-if(NOT EXISTS "${BIB2WEB_SHELLCHECK}")
-	message("shellcheck does not exist. No Bash linting will be added to the build.")
-	set(BIB2WEB_SHELLCHECK_EXISTS FALSE)
-else()
-	set(BIB2WEB_SHELLCHECK_EXISTS TRUE)
-endif()
+# Pre-check that shunit2 is available
+checkTool(shunit2)
+
+# Function to set up shell tests using shunit2
+function(setupShellTests shell generator test_files)
+	# Add target for running the tests, if possible
+	if(BIB2WEB_shunit2_EXISTS)
+		# Generate the master test file to be run
+		set("${shell}_TEST_MASTER_FILE" "${CMAKE_CURRENT_BINARY_DIR}/.${shell}-test.sh")
+		add_custom_command(
+			OUTPUT "${${shell}_TEST_MASTER_FILE}"
+			COMMAND "/bin/${shell}" ARGS "${generator}" "${CMAKE_CURRENT_SOURCE_DIR}/../src" "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}" "${BIB2WEB_shunit2}" "${${shell}_TEST_MASTER_FILE}" ${test_files}
+			DEPENDS "${generator}" "${test_files}")
+		# Custom target for running the test
+		add_custom_target("${shell}-test"
+			DEPENDS "${${shell}_TEST_MASTER_FILE}")
+		add_custom_command(
+			TARGET "${shell}-test"
+			COMMAND "/bin/${shell}" ARGS "${${shell}_TEST_MASTER_FILE}")
+		# Add the testing target as a dependency to the general testing target
+		add_dependencies(test "${shell}-test")
+	else()
+		message("Skipping ${shell} shell tests...")
+	endif()
+endfunction()
+
+# Pre-check that shellcheck is available
+checkTool(shellcheck)
+
+# Function to running shell linter using shellcheck
+function(setupShellLinting target_name dependency files)
+	# Add target for running shellcheck, if possible
+	if (BIB2WEB_shellcheck_EXISTS)
+		add_custom_target("${target_name}-lint"
+			DEPENDS "${files}"
+			)
+		add_custom_command(TARGET "${target_name}-lint"
+			COMMAND "${BIB2WEB_shellcheck}" ARGS "-x" ${files}
+			WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+		# Add the shell linting target as a dependency to the general linting target
+		add_dependencies(lint "${target_name}-lint")
+		# Add the dependency to this newly created target
+		if (NOT "${dependency}" STREQUAL "")
+			add_dependencies("${target_name}-lint" "${dependency}")
+		endif()
+	endif()
+endfunction()
