09f5bbd418cba80bb17b5001a3ad8e5dba273b6d
Jussi Pekonen <jussi@pekonen.cc>
2022-06-15 23:20:43 +0300

    bash: Use  for finding the tools

--- bash/src/preprocessor.bash
+++ bash/src/preprocessor.bash
@@ -7,93 +7,59 @@ source "${BIB2WEB_BASE_DIR}/logger.bash"
 # shellcheck source=./error-codes.bash
 source "${BIB2WEB_BASE_DIR}/error-codes.bash"
 
-getToolFinder() {
-	local toolPath
-	local toolResult
-	# Try which
-	toolPath=$(which which)
-	toolResult="$?"
-	if [ "${toolResult}" == "0" ] && [ "${toolPath}" != "" ]; then
-		echo "which"
-		return 0
-	fi
-	# Try whereis
-	toolPath=$(whereis whereis)
-	toolResult="$?"
-	if [ "${toolResult}" == "0" ] && [ "${toolPath}" != "" ]; then
-		echo "whereis"
-		return 0
-	fi
-	# Try locate
-	toolPath=$(locate locate)
-	toolResult="$?"
-	if [ "${toolResult}" == "0" ] && [ "${toolPath}" != "" ]; then
-		echo "locate"
-		return 0
-	fi
-	return "${BIB2WEB_TOOL_FINDER_NOT_FOUND}"
-}
-
 # Set up tools
 setUpTools() {
-	local toolFinder
-	toolFinder=$(getToolFinder)
-	local toolFinderResult="$?"
-	if [ "${toolFinderResult}" -gt "0" ]; then
-		printError "No tool finder tool available!"
-		return "${toolFinderResult}"
-	fi
 	# basename
-	BIB2WEB_BASENAME=$("${toolFinder}" "basename")
+	BIB2WEB_BASENAME=$(command -v "basename")
 	local basenameResult="$?"
 	if [ "${basenameResult}" -gt 0 ]; then
 		printError "basename not found!"
 		return "${BIB2WEB_BASENAME_NOT_FOUND}"
 	fi
 	# grep
-	BIB2WEB_GREP=$("${toolFinder}" "grep")
+	BIB2WEB_GREP=$(command -v "grep")
 	local grepResult="$?"
 	if [ "${grepResult}" -gt 0 ]; then
 		printError "grep not found!"
 		return "${BIB2WEB_GREP_NOT_FOUND}"
 	fi
 	# awk
-	BIB2WEB_AWK=$("${toolFinder}" "awk")
+	BIB2WEB_AWK=$(command -v "awk")
 	local awkResult="$?"
 	if [ "${awkResult}" -gt 0 ]; then
 		printError "awk not found!"
 		return "${BIB2WEB_AWK_NOT_FOUND}"
 	fi
 	# mktemp
-	BIB2WEB_MKTEMP=$("${toolFinder}" "mktemp")
+	BIB2WEB_MKTEMP=$(command -v "mktemp")
 	local mktempResult="$?"
 	if [ "${mktempResult}" -gt 0 ]; then
 		printError "mktemp not found!"
 		return "${BIB2WEB_MKTEMP_NOT_FOUND}"
 	fi
 	# rm
-	BIB2WEB_RM=$("${toolFinder}" "rm")
+	BIB2WEB_RM=$(command -v "rm")
 	local rmResult="$?"
 	if [ "${rmResult}" -gt 0 ]; then
 		printError "rm not found!"
 		return "${BIB2WEB_RM_NOT_FOUND}"
 	fi
 	# touch
-	BIB2WEB_TOUCH=$("${toolFinder}" "touch")
+	BIB2WEB_TOUCH=$(command -v "touch")
 	local touchResult="$?"
 	if [ "${touchResult}" -gt 0 ]; then
 		printError "touch not found!"
 		return "${BIB2WEB_TOUCH_NOT_FOUND}"
 	fi
 	# cat
-	BIB2WEB_CAT=$("${toolFinder}" "cat")
+	BIB2WEB_CAT=$(command -v "cat")
 	local catResult="$?"
 	if [ "${catResult}" -gt 0 ]; then
 		printError "cat not found!"
 		return "${BIB2WEB_CAT_NOT_FOUND}"
 	fi
 	# find
-	BIB2WEB_FIND=$("${toolFinder}" "find")
+	BIB2WEB_FIND=$(command -v "find")
 	local findResult="$?"
 	if [ "${findResult}" -gt 0 ]; then
 		printError "find not found!"
